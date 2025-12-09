94dbbc806b3e8fa2f5963d937aa3a9d9309abc9f
Tiflit <59609757+Tiflit@users.noreply.github.com>
2025-12-09 11:07:51 -0500

    Update openssl_ui_stubs.c

--- android/app/src/main/cpp/openssl_ui_stubs.c
+++ android/app/src/main/cpp/openssl_ui_stubs.c
@@ -1,4 +1,9 @@
 // android/app/src/main/cpp/openssl_ui_stubs.c
 #include <stdio.h>
+
+// Android NDK defines stdin/stderr as macros; undef them so we can provide linker symbols.
+#undef stdin
+#undef stderr
+
 FILE *stdin = NULL;
 FILE *stderr = NULL;
